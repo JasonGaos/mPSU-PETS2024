3fb2b50c7584ebd9681b4def9230cc0d0ecfd667
JasonGaos <31381946+JasonGaos@users.noreply.github.com>
2023-09-14 16:40:28 -0700

    multi-thread

--- mpsu.cpp
+++ mpsu.cpp
@@ -6,19 +6,28 @@
 #include "oprf_mpsu.h"
 #include "libOTe/libOTe_Tests/OT_Tests.h"
 #include "hash_test.h"
+#include "chls_test.h"
+#include "oprf_batch_mpsu.h"
 
 using namespace osuCrypto;
 
 int main(void){
 
+
     mpsu_test();
+    // makeup_test();
+    // sspmt_test();
+    // oprf_batched_test();
+    // channel_delay_test();
     //rpir_framework_test();
-    //ecc_channel_test();
-    //oprf_test();
-    //gc_test();
+    // ecc_channel_test();
+    // convert_test();
+    // oprf_test();
+    // gc_test();
     //OtExt_Iknp_Test();
     // ot_test();
     //hash_test();
+
 
     return 0;
 }
